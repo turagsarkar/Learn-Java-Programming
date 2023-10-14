452e8c702956ec2f42d7ee6ffad247dc5ddde371
Md.Abdullah Turag Sarkar <turagsarkar@gmail.com>
2023-10-14 22:13:41 +0600

    left rotate the elements of an array

--- Single Dimensional Arrays/src/Shifting.java
+++ Single Dimensional Arrays/src/Shifting.java
@@ -1,5 +1,13 @@
 public class Shifting {
     public static void main(String[] args) {
-        
+        int[] number= {1,2,3,4,5};
+        int first = number[0];
+        for(int i = 0 ; i< number.length-1;i++){
+            number[i]=number[i+1];
+        }
+        number[number.length-1]=first;
+        for (int i = 0 ; i < number.length;i++ ){
+            System.out.println(number[i]);
+        }
     }
 }
