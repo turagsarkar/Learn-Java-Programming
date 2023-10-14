13e3eeca7b7265d8eaf388e13051b028c1999563
Md.Abdullah Turag Sarkar <turagsarkar@gmail.com>
2023-10-14 21:08:40 +0600

    Single-Dimensional Arrays

---
+++ Single Dimensional Arrays/src/Arrays.java
@@ -0,0 +1,8 @@
+public class Arrays {
+    public static void array() {
+        int[] number = {4, 5, 6, 7, 8};//(Initializing arrays with random values)
+        for (int i = 0; i < number.length; i++) {
+            System.out.println(number[i]);
+        }
+    }
+}
---
+++ Single Dimensional Arrays/src/Sumbyarray.java
@@ -0,0 +1,2 @@
+public class Sumbyarray {
+}
