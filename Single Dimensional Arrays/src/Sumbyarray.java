083bdd8c169b6d0cad28c705e7810f1166d47234
Md.Abdullah Turag Sarkar <turagsarkar@gmail.com>
2023-10-14 21:15:09 +0600

    Single Dimensional Arrays

--- Single Dimensional Arrays/src/Sumbyarray.java
+++ Single Dimensional Arrays/src/Sumbyarray.java
@@ -1,2 +1,12 @@
 public class Sumbyarray {
+    public static void sum(){
+        int[] number = {4, 5, 6, 7, 8};//(Initializing arrays with random values)
+        int sum = 0 ;
+
+        for (int i = 0; i < number.length; i++) {
+            sum+=number[i];
+        }
+        System.out.println(sum);
+
+    }
 }
