9001f47b1855cfbd3b12c5f1f5c940b2d1569c19
Md.Abdullah Turag Sarkar <turagsarkar@gmail.com>
2023-10-17 00:48:56 +0600

    problem solving task

--- Single Dimensional Arrays/src/Main.java
+++ Single Dimensional Arrays/src/Main.java
@@ -2,9 +2,9 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        //Arrays.array();
+        Arrays.array();
         //Sumbyarray.sum();
         //LargeNumber.lnum();
-        RandomValue.rand();
+       //RandomValue.rand();
     }
 }
